d718103a6c39498fc5b84430b8b986401e8f96b2
Wegii <ge36boj@mytum.de>
2019-11-24 00:54:17 +0100

    Add error handling if no internet connection available

--- gitcommit/renderer/message/message.py
+++ gitcommit/renderer/message/message.py
@@ -1,9 +1,7 @@
-import requests
-import logging
+import logging, requests
 from gitcommit.renderer import BaseRenderer
 
 logger = logging.getLogger("base_renderer")
-
 
 class MessageRenderer(BaseRenderer):
     def __self__(self, variables, config):
@@ -16,8 +14,13 @@ class MessageRenderer(BaseRenderer):
         Returns: 
             data: dict containig random message
         """
-        response = requests.get("http://whatthecommit.com/index.txt")
-        data = {"message": response.content.decode("utf-8")}
+        try:
+            response = requests.get('http://whatthecommit.com/index.txt')
+            content = response.content.decode('utf-8')
+        except requests.exceptions.RequestException as e:
+            content = None
+
+        data = {"message": content}
 
         return data
 
